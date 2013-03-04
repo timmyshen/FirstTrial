d164aa221652925d6607a643c4a44b4fff5380cf
Benqing Shen <benqingshen@gmail.com>
2013-03-03 22:58:40 -0600

    first commit git

---
+++ ConsoleApplication1/Win32ConsoleApp/Source.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+
+//#include <boost\lambda\lambda.hpp>
+
+//#include <ql\timeseries.hpp>
+//#include <ql\quantlib.hpp>
+
+#include "C:\BenqingShen\ProgrammingLibs\ALGLIB\cpp\src\ap.h"
+
+using namespace std;
+
+int main()
+{
+	cout<<"Hello" << endl;
+
+	//using namespace boost::lambda;
+	//typedef std::istream_iterator<int> in;
+
+	//for_each(
+	//	in(cin), in(), cout << (_1*3) << " " );
+
+	//QuantLib::TimeSeries<double> timeseries;
+	//QuantLib::Date today(23, QuantLib::Feb, 2013);
+	//cout<< today << endl;
+
+
+}
